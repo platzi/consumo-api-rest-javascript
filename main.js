2e3ab8ae336e15bd7b47d0293b622ebc4b0d8d58
Juan David Castro <me@juandc.co>
2022-03-01 13:27:45 -0500

    Clase 08: get

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
-console.log('Hello, world')
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
+const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=c08d415f-dea7-4a38-bb28-7b2188202e46';
+const spanError = document.getElementById('error')
 
-async function reload() {
-  const res = await fetch(API_URL);
+async function loadRandomMichis() {
+  const res = await fetch(API_URL_RANDOM);
   const data = await res.json();
-
+  console.log('Random')
   console.log(data)
-  const img1 = document.getElementById('img1');
-  const img2 = document.getElementById('img2');
-  const img3 = document.getElementById('img3');
-  
-  img1.src = data[0].url;
-  img2.src = data[1].url;
-  img3.src = data[2].url;
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  } else {
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+  }
 }
 
-reload();
+async function loadFavoritesMichis() {
+  const res = await fetch(API_URL_FAVOTITES);
+  const data = await res.json();
+  console.log('Favoritos')
+  console.log(data)
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  }
+}
+
+loadRandomMichis();
+loadFavoritesMichis();
