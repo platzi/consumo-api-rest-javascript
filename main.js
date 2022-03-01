2b9e1ebb3227988bcee14d9db03e57a7ddcef968
Juan David Castro <me@juandc.co>
2022-03-01 12:06:39 -0500

    Clase 03: endpoints y query parameters

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 console.log('Hello, world')
 
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
 
-fetch(URL)
-  .then(res => res.json())
-  .then(data => {
-    const img = document.querySelector('img');
-    img.src = data[0].url;
-  });
+async function reload() {
+  const res = await fetch(API_URL);
+  const data = await res.json();
+
+  console.log(data)
+  const img1 = document.getElementById('img1');
+  const img2 = document.getElementById('img2');
+  const img3 = document.getElementById('img3');
+  
+  img1.src = data[0].url;
+  img2.src = data[1].url;
+  img3.src = data[2].url;
+}
+
+reload();
