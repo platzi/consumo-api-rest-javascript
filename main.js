6dd91017a05b2b212106761d41551109d8e01f9c
Juan David Castro <me@juandc.co>
2022-03-02 09:50:45 -0500

    Clase 10: manipulación dinámica del DOM

--- main.js
+++ main.js
@@ -14,9 +14,14 @@ async function loadRandomMichis() {
   } else {
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
+    const btn1 = document.getElementById('btn1');
+    const btn2 = document.getElementById('btn2');
     
     img1.src = data[0].url;
     img2.src = data[1].url;
+
+    btn1.onclick = () => saveFavouriteMichi(data[0].id);
+    btn2.onclick = () => saveFavouriteMichi(data[1].id);
   }
 }
 
@@ -28,17 +33,32 @@ async function loadFavouriteMichis() {
 
   if (res.status !== 200) {
     spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    data.forEach(michi => {
+      const section = document.getElementById('favoriteMichis')
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      const btn = document.createElement('button');
+      const btnText = document.createTextNode('Sacar al michi de favoritos');
+
+      img.src = michi.image.url;
+      img.width = 150;
+      btn.appendChild(btnText);
+      article.appendChild(img);
+      article.appendChild(btn);
+      section.appendChild(article);
+    });
   }
 }
 
-async function saveFavouriteMichis() {
+async function saveFavouriteMichi(id) {
   const res = await fetch(API_URL_FAVOTITES, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      image_id: 'dje'
+      image_id: id
     }),
   });
   const data = await res.json();
